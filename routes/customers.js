8f81925c216def29f6464b7fabc9fbe3068ccac4
awais-ur-rehman <awaisjarral37@gmail.com>
2024-06-12 15:26:11 +0500

    update order-api

--- routes/customers.js
+++ routes/customers.js
@@ -96,8 +96,8 @@ router.get("/get-order", async (req, res) => {
       deliveryDay: deliveryDay,
     }).lean();
 
-    res.json(customers);
     res.status(200).json({ message: "Received" });
+    res.json(customers);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
